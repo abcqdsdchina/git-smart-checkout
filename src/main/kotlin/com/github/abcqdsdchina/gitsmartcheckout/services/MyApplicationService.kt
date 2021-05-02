0d68ba03ad8e993eefa1205e94dec9af6212d40b
GitHub Action <action@github.com>
2021-05-02 05:41:42 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/abcqdsdchina/gitsmartcheckout/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.abcqdsdchina.gitsmartcheckout.services
+
+import com.github.abcqdsdchina.gitsmartcheckout.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/abcqdsdchina/gitsmartcheckout/services/MyProjectService.kt
+++ src/main/kotlin/com/github/abcqdsdchina/gitsmartcheckout/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.abcqdsdchina.gitsmartcheckout.services
 
+import com.github.abcqdsdchina.gitsmartcheckout.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
